7dc1345d63d1b0498d83c69a830d582fb80d6ca3
shouryam24bce10020-lab <shouryam.24bce10020@vitbhopal.ac.in>
2026-03-30 21:55:42 +0530

    Create script1_system_identity.sh

---
+++ script1_system_identity.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Script 1: System Identity Report
+# Author: Shouryam Chaturvedi | Reg No: 24BCE10020
+
+echo "=========================================="
+echo "       OPEN SOURCE AUDIT PROJECT"
+echo "=========================================="
+echo "Student Name : Shouryam Chaturvedi"
+echo "Reg Number   : 24BCE10020"
+echo "Audit Target : VLC Media Player"
+echo "------------------------------------------"
+echo "OS Distro    : JSLinux (Buildroot)"
+echo "Kernel       : $(uname -r)"
+echo "User         : $(whoami)"
+echo "Date/Time    : $(date)"
+echo "=========================================="
