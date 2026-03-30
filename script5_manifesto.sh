ec1dd984c3ad522340a7c8116aed0857e85e00f3
shouryam24bce10020-lab <shouryam.24bce10020@vitbhopal.ac.in>
2026-03-30 21:58:14 +0530

    Create script5_manifesto.sh

---
+++ script5_manifesto.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Script 5: Open Source Manifesto Generator
+echo "Running Manifesto Generator..."
+# Note: For non-interactive GitHub storage, we show the logic
+echo "I use VLC to build the future." > manifesto.txt
+echo "To me, Open Source is Innovation." >> manifesto.txt
+cat manifesto.txt
