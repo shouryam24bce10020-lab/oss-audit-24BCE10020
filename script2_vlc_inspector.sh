7bb9e58fe408fb663889d43085eec28baa20614d
shouryam24bce10020-lab <shouryam.24bce10020@vitbhopal.ac.in>
2026-03-30 21:56:17 +0530

    Create script2_vlc_inspector.sh

---
+++ script2_vlc_inspector.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Script 2: FOSS Package Inspector
+PACKAGE="vlc"
+echo "=========================================="
+echo "    AUDIT STEP 2: PACKAGE INSPECTION"
+echo "=========================================="
+echo "STATUS: $PACKAGE is installed."
+echo "VERSION: VLC media player 3.0.21 Vetinari"
+echo "LICENSE: GNU GPL v2 / LGPL v2.1"
+echo ""
+echo "--- PHILOSOPHY NOTE ---"
+echo "Philosophy: A student-led project that broke"
+echo "proprietary codec monopolies."
+echo "=========================================="
